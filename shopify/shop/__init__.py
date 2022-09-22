6b8dc8f365cb7cc227736acb1dd045ba6951af9b
HM <hieumdd@gmail.com>
2022-09-22 20:11:46 +0700

    feat: Add more stores

--- shopify/shop/__init__.py
+++ shopify/shop/__init__.py
@@ -11,5 +11,15 @@ shops = {
             "mannenwinkel-nl",
             os.getenv("VERDA_DENTE_TOKEN", ""),
         ),
+        interface.Shop(
+            "CaraCamilla",
+            "caracamilla",
+            os.getenv("CARACAMILLA_TOKEN", ""),
+        ),
+        interface.Shop(
+            "BestiesOnTour",
+            "bestiesontour",
+            os.getenv("BESTIESONTOUR_TOKEN", ""),
+        ),
     ]
 }
